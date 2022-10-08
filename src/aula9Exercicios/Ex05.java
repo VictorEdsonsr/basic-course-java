a2434abec886e3e64554e2f37b3756926f14693f
Victor <edvictorsr@gmail.com>
2022-10-07 20:46:36 -0400

    agenda dia/hora -- agenda mes/dia/hora

--- src/aula9Exercicios/Ex05.java
+++ src/aula9Exercicios/Ex05.java
@@ -2,12 +2,12 @@ package aula9Exercicios;
 
 import java.util.Scanner;
 
-public class Ex04 {
+public class Ex05 {
 
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
-		String[][] agenda = new String[31][24];
+		String[][][] agenda = new String[12][31][9];
 		
 		boolean sair = true;
 		int opcao;
@@ -21,16 +21,27 @@ public class Ex04 {
 
 				
 			if(opcao == 1) {
-			int dia = 0;
-			int hora = 0;
-			boolean diaHoraValida = true;
+				int mes = 0;
+				int dia = 0;
+				int hora = 0;
 				
-					while(diaHoraValida) {
+				boolean diaHoraMesValido = true;
+				
+					while(diaHoraMesValido) {
+						System.out.println("Digite o mes");
+						mes = input.nextInt();
+						
+						if(mes > 0 && dia <= 12) {
+							diaHoraMesValido = false;
+						}else {
+							System.out.println("Mes invalido digite novamente.");
+						}
+						
 						System.out.println("Digite o dia");
 						dia = input.nextInt();
 						
 						if(dia > 0 && dia <= 31) {
-							diaHoraValida = false;
+							diaHoraMesValido = false;
 						}else {
 							System.out.println("Dia invalido digite novamente.");
 						}
@@ -38,29 +49,41 @@ public class Ex04 {
 						
 						System.out.println("Digite a hora");
 						hora = input.nextInt();
-						if(hora >= 0 && hora <= 23) {
-							diaHoraValida = false;
+						if(hora >= 0 && hora <= 8) {
+							diaHoraMesValido = false;
 						}else {
 							System.out.println("Hora invalida digite novamente.");
 						}
 					}
-					
+					mes--;
+					dia--;
 					System.out.println("Digite o seu compromisso");
-					agenda[dia][hora] = input.next();
+					agenda[mes][dia][hora] = input.next();
 					
 				
 				
 			}else if(opcao == 2) {
+				int mes = 0;
 				int dia = 0;
 				int hora = 0;
-				boolean diaHoraValida = true;
 				
-				while(diaHoraValida) {
+				boolean diaHoraMesValido = true;
+				
+				while(diaHoraMesValido) {
+					System.out.println("Digite o mes");
+					mes = input.nextInt();
+					
+					if(mes > 0 && dia <= 12) {
+						diaHoraMesValido = false;
+					}else {
+						System.out.println("Mes invalido digite novamente.");
+					}
+					
 					System.out.println("Digite o dia");
 					dia = input.nextInt();
 					
 					if(dia > 0 && dia <= 31) {
-						diaHoraValida = false;
+						diaHoraMesValido = false;
 					}else {
 						System.out.println("Dia invalido digite novamente.");
 					}
@@ -68,14 +91,15 @@ public class Ex04 {
 					
 					System.out.println("Digite a hora");
 					hora = input.nextInt();
-					if(hora >= 0 && hora <= 23) {
-						diaHoraValida = false;
+					if(hora >= 0 && hora <= 8) {
+						diaHoraMesValido = false;
 					}else {
 						System.out.println("Hora invalida digite novamente.");
 					}
 				}
-				
-				System.out.println("Compromisso --> " + agenda[dia][hora]);
+				mes--;
+				dia--;
+				System.out.println("Compromisso --> " + agenda[mes][dia][hora]);
 			}else if (opcao == 0) {
 				sair = false;
 			}else {
